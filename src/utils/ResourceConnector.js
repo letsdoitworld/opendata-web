5f7009e524683bf3b872db4bef7551de7f5b447c
eugen.knyazev <eugen.knyazev@wemakesoftware.eu>
2018-05-09 12:01:07 +0300

    country lists with names

--- src/utils/ResourceConnector.js
+++
@@ -1,9 +0,0 @@
-export function getJSOnFromApiAsync(apiURl) {
-    return fetch(apiURl)
-        .then(response => response.json())
-        .then(responseJson => responseJson)
-        .catch((error) => {
-            console.error(error);
-        });
-}
-export default {getJSOnFromApiAsync};
