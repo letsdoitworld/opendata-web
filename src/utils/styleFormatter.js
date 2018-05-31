2b969a0a43939f328a08208b38d207f250786583
eugen.knyazev <eugen.knyazev@wemakesoftware.eu>
2018-05-31 16:02:34 +0300

    dot styles changed

--- src/utils/styleFormatter.js
+++ src/utils/styleFormatter.js
@@ -1,6 +1,24 @@
 export const COLORS = ['#fcde9c', '#faa476', '#f0746e', '#e34f6f', '#dc3977', '#b9257a', '#7c1d6f'];
 function _createRule() {
     return `
+            [hazardous=true] {
+            [status='REPORTED'] {
+                marker-fill: #7c1d6f;
+                marker-line-color: #FF548B;
+                marker-line-width: 2;
+            }
+            [status='CONFIRMED'] {
+                marker-fill: #7c1d6f;
+                marker-line-color: #FF548B;
+                marker-line-width: 2;
+            }
+            [status='CLEANED'] {
+                marker-fill: #7c1d6f;
+                marker-line-color: #50E3C2;
+                marker-line-width: 2;
+            }
+            }
+            [hazardous=false] {
             [status='REPORTED'] {
                 marker-fill: #FF548B;
             }
@@ -9,6 +27,7 @@ function _createRule() {
             }
             [status='CLEANED'] {
                 marker-fill: #50E3C2;
+            }
             }
         `;
 }
