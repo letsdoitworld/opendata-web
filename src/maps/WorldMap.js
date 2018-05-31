257c0be2fd3aeda679c4881a1416917ab5d85fcc
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2018-05-31 14:13:58 +0300

    added markers to maps

--- src/maps/WorldMap.js
+++ src/maps/WorldMap.js
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {withRouter} from 'react-router';
-import {Map, TileLayer as Basemap} from 'react-leaflet';
+import {Map, Marker, TileLayer as Basemap} from 'react-leaflet';
 import carto from 'carto.js';
 import cartoMapData from '../data/cartoMapData';
 import Layer from './Layer';
 import {buildStyle} from '../utils/styleFormatter';
+
+// this is a workaround for https://github.com/PaulLeCam/react-leaflet/issues/255#issuecomment-261904061
+import L from 'leaflet'; // eslint-disable-line
+
+delete L.Icon.Default.prototype._getIconUrl;/**/
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'), // eslint-disable-line
+    iconUrl: require('leaflet/dist/images/marker-icon.png'), // eslint-disable-line
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'), // eslint-disable-line
+});
+
+// end of workaround
 
 const CARTO_BASEMAP = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png';
 
@@ -30,23 +43,36 @@ class WorldMap extends Component {
             nativeMap: undefined,
             layerStyle: cartoMapData.style,
             hidelayers: true,
-            cartoClient: new carto.Client({apiKey: '7947aa9e7fcdff0f5f8891a5f83b1e6fa6350687', username: 'worldcleanupday'}),
+            cartoClient: new carto.Client({
+                apiKey: '7947aa9e7fcdff0f5f8891a5f83b1e6fa6350687',
+                username: 'worldcleanupday',
+            }),
+            marker: null,
         };
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.selectedTrashPoint
-            && nextProps.selectedTrashPoint
-            !== this.props.selectedTrashPoint) {
+            && nextProps.selectedTrashPoint !== this.props.selectedTrashPoint) {
             const center = [nextProps.selectedTrashPoint.lat, nextProps.selectedTrashPoint.long];
 
             this.nativeMap.panTo(center);
+
+            if (this.state.marker) {
+                this.nativeMap.removeLayer(this.state.marker);
+            }
+            const marker = Marker.prototype.createLeafletElement(
+                {position: center, pane: this.nativeMap.getPane('markerPane')});
+            this.setState({marker});
+            marker.addTo(this.nativeMap);
             return true;
         }
         return false;
     }
 
     shouldComponentUpdate() {
+        // always return false otherwise CARTO will complain.
+        // all interactions with the map are done programmatically inside componentWillReceiveProps
         return false;
     }
 
