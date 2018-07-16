c3256df4e6b244eb2f92db6676e430790e94008d
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2018-07-16 18:07:06 +0300

    About page updated

--- src/AboutProject.js
+++ src/AboutProject.js
@@ -12,56 +12,70 @@ export default class AboutProject extends Component {
         };
     }
 
-
+/* eslint-disable */
     render() {
         return (
             <div className={this.props.aboutClassName}>
                 <main className="main-content about-page">
-                    <header className="header">
-                        <h1 className="header__text">Mismanaged trash reports</h1>
-                    </header>
-
-                    <section className="section">
-                        <h3 className="intro">Why we created this platform</h3>
-                        <p>
-                            Let’s Do It! World is a civic-led mass movement that began
-                            in Estonia in 2008 when 50,000 people united together to clean up
-                            the entire country in just five hours. Since then, Let’s Do It! has
-                            spread this model—one country in one day—around the world. To date,
-                            nearly 120 countries and 20 million people have joined us to clean
-                            up illegal waste.
+                    <div className="modal-content"><img src="img/about/letsdoitworld.svg" alt="Let's do it world!"/>
+                        <p className="center">
+                            Powered by
+                            <a target="_blank" rel="noreferrer noopener" href="https://www.letsdoitworld.org">Let’s Do It Foundation</a>
                         </p>
-
-                        <h3 className="intro">Be aware of the whole world</h3>
-                        <p>
-                            Let’s Do It! World is a civic-led mass movement that began in Estonia
-                             in 2008 when 50,000 people united together to clean up the entire
-                              country in just five hours. Since then, Let’s Do It! has spread
-                               this model—one country in one day—around the world. To date, nearly
-                                120 countries and 20 million people have joined us to clean up
-                                 illegal waste.
+                        <p className="center tighter"><span>Platform is developed by </span>
+                            <a target="_blank" rel="noreferrer noopener" href="https://wemakesoftware.eu/">Idakoidlik O&Uuml;</a>
+                            <span> and </span>
+                            <a target="_blank" rel="noreferrer noopener" href="http://www.infovara.ee/">Infovara</a>
+                            <span> in partnership with </span>
+                            <a target="_blank" rel="noreferrer noopener" href="http://www.qlik.com/us">Qlik</a>
                         </p>
-                    </section>
-
-                    <div className="header">
-                        <h2 className="header__text">World Waste Index</h2>
+                        <p><b>A world without waste is our dream.</b>
+                            <br/>
+                            <br/>
+                            In order to fully grasp the trash problem, we require reliable data on how much trash there
+                             is in the world - and we understood that there is no such database to collect this kind of data.
+                              And so the World Waste Platform was born. We have put together&nbsp;
+                            <a
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                href="https://opendata.letsdoitworld.org/analytics/single/?appid=3d7e26a3-298b-4cb8-aef9-80ec65196542&amp;sheet=jjsmF&amp;opt=currsel&amp;select=country,World"
+                            >
+                                data from eight trash mapping apps
+                            </a> , data compiled by&nbsp;
+                            <a
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                href="http://andrewgray.ucr.edu/"
+                            >Gray’s Lab</a>
+                            &nbsp;and we will continue this quest until all the trash is mapped. </p>
+                        <p><span>For more information please contact </span>
+                            <a href="mailto:opendata@letsdoitworld.org">opendata@letsdoitworld.org</a>
+                        </p><p><span>Media Inquiries </span>
+                            <a href="mailto:tiina.urm@letsdoitworld.org">tiina.urm@letsdoitworld.org</a></p>
+                        <p>
+                                <span>Technical description is available: </span>
+                            <a target="_blank" rel="noreferrer noopener" href="https://github.com/letsdoitworld">
+                                github.com/letsdoitworld
+                            </a>
+                            </p><p>
+                            Funders: Estonian Ministry of Environment, The Ministry of Foreign Affairs of the Republic of Estonia, Estonian Republic 100 program</p>
+                        <p>
+                            <span>You can make a difference! Start mapping illegal trash and together we can clean it on </span><a
+                            target="_blank" rel="noreferrer noopener" href="https://worldcleanupday.org">World Cleanup Day 15 September 2018</a>
+                        </p>
+                        <small>
+                            <b>Disclaimer:</b>
+                            Users are solely liable for how they use the information about trash locations provided in the platform, including their activities if they access or remove the trash. LDI is not liable for the activities of users who access or remove the trash. Publication of a trash location on the platform does not constitute an approval or request by LDI to remove the trash. Users acknowledge that accessing trash locations published on the platform and removing the trash may be dangerous or illegal. Our map data relies on
+                            <a className="no-highlight" target="_blank" rel="noreferrer noopener" href="https://www.openstreetmap.org/">
+                                OpenStreetMap
+                            </a>and population on
+                            <a className="no-highlight" target="_blank" rel="noreferrer noopener" href="http://api.population.io/">
+                                World Population API
+                            </a> data.
+                            <br/> Additional sources: NOAA, Clean Cost Index.</small>
                     </div>
-
-                    <section className="section">
-                        <h3>What is World Waste Index</h3>
-                        <p>
-                            Let’s Do It! World is a civic-led mass movement that began in Estonia
-                             in 2008 when 50,000 people united together to clean
-                              up the entire country
-                              in just five hours. Since then, Let’s Do It! has spread this model—one
-                               country in one day—around the world. To date, nearly 120 countries
-                                and 20 million people have joined us to clean up illegal waste.
-                        </p>
-                        <h3>How it’s calculated</h3>
-                    </section>
-
                 </main>
             </div>
         );
     }
-}
+} /* eslint-enable */
